3f00bbf63d46164fe07a762c957fa2714309a820
OmarLaborin <L21330392@hermosillo.tecnm.mx>
2025-05-01 11:06:05 -0700

    Commit después de reiniciar el índice

--- src/app/lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-// lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://xakntjlnhxwpmiwzrala.supabase.co"; // Sustituye
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhha250amxuaHh3cG1pd3pyYWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMjUzNzAsImV4cCI6MjA2MDYwMTM3MH0.ofpudYh-PLiRbVsSTXobGHpZSmNlorEbPB4raXuLJIc"; // Sustituye
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
